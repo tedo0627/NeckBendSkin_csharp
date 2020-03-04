f45da755409a553c53b3a9390a714052dfbbc75c
tedo0627 <tedosw1mpr1ver@gmail.com>
2020-03-05 03:44:17 +0900

    アップロード

---
+++ NeckBendSkin/NeckBendSkin/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace NeckBendSkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.basePath = new System.Windows.Forms.TextBox();
+            this.execute = new System.Windows.Forms.Button();
+            this.selectPath1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.createPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectPath2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // basePath
+            // 
+            this.basePath.Location = new System.Drawing.Point(43, 68);
+            this.basePath.Name = "basePath";
+            this.basePath.Size = new System.Drawing.Size(308, 19);
+            this.basePath.TabIndex = 0;
+            // 
+            // execute
+            // 
+            this.execute.Location = new System.Drawing.Point(43, 205);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(75, 23);
+            this.execute.TabIndex = 1;
+            this.execute.Text = "実行";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // selectPath1
+            // 
+            this.selectPath1.Location = new System.Drawing.Point(357, 67);
+            this.selectPath1.Name = "selectPath1";
+            this.selectPath1.Size = new System.Drawing.Size(20, 20);
+            this.selectPath1.TabIndex = 2;
+            this.selectPath1.Text = "...";
+            this.selectPath1.UseVisualStyleBackColor = true;
+            this.selectPath1.Click += new System.EventHandler(this.SelectPath1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "元のスキンを選択してください";
+            // 
+            // createPath
+            // 
+            this.createPath.Location = new System.Drawing.Point(43, 150);
+            this.createPath.Name = "createPath";
+            this.createPath.Size = new System.Drawing.Size(308, 19);
+            this.createPath.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "出力先を選択してください";
+            // 
+            // selectPath2
+            // 
+            this.selectPath2.Location = new System.Drawing.Point(357, 150);
+            this.selectPath2.Name = "selectPath2";
+            this.selectPath2.Size = new System.Drawing.Size(20, 20);
+            this.selectPath2.TabIndex = 6;
+            this.selectPath2.Text = "...";
+            this.selectPath2.UseVisualStyleBackColor = true;
+            this.selectPath2.Click += new System.EventHandler(this.SelectPath2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 333);
+            this.Controls.Add(this.selectPath2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.createPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectPath1);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.basePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox basePath;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button selectPath1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox createPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectPath2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ NeckBendSkin/NeckBendSkin/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NeckBendSkin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectPath1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "png files (*.png)|*.png";
+            openFileDialog1.Title = "スキンの画像を選択してください";
+            openFileDialog1.FileName = "skin.ping";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                String text = openFileDialog1.FileName;
+                basePath.Text = text;
+                if (text.Contains(".png") && createPath.Text == "")
+                {
+                    createPath.Text = text.Replace(".png", "_neck_bend.png");
+                }
+            }
+        }
+
+        private void SelectPath2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "png files (*.png)|*.png";
+            openFileDialog1.Title = "スキンの保存先の画像を選択してください";
+            openFileDialog1.FileName = "skin.ping";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                createPath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            if (basePath.Text == "")
+            {
+                MessageBox.Show("元のスキンを選択してください");
+                return;
+            }
+
+            if (!File.Exists(basePath.Text))
+            {
+                MessageBox.Show("元のスキンが見つかりませんでした");
+                return;
+            }
+
+            if (createPath.Text == "")
+            {
+                MessageBox.Show("保存先を選択してください");
+                return;
+            }
+
+            Bitmap read = new Bitmap(basePath.Text);
+            Bitmap write = new Bitmap(basePath.Text);
+
+            PutTop(read, write, true);
+            PutTop(read, write, false);
+            PutDown(read, write, true);
+            PutDown(read, write, false);
+            PutFront(read, write, true);
+            PutFront(read, write, false);
+            PutBack(read, write, true);
+            PutBack(read, write, false);
+            PutRight(read, write, true);
+            PutRight(read, write, false);
+            PutLeft(read, write, true);
+            PutLeft(read, write, false);
+
+            write.Save(createPath.Text);
+            MessageBox.Show("処理が完了しました");
+
+        }
+
+        private void PutTop(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + 16 + (outSide ? 32 : 0), y + 8));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + 8 + (outSide ? 32 : 0), y, cut.GetPixel(x, y));
+                }
+            }
+        }
+
+        private void PutDown(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + (outSide ? 32 : 0), y + 8));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + 16 + (outSide ? 32 : 0), y, cut.GetPixel(7 - x, y));
+                }
+            }
+        }
+
+        private void PutFront(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + 8 + (outSide ? 32 : 0), y + 8));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + 8 + (outSide ? 32 : 0), y + 8, cut.GetPixel(x, y));
+                }
+            }
+        }
+
+        private void PutBack(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + 24 + (outSide ? 32 : 0), y + 8));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + 24 + (outSide ? 32 : 0), y + 8, cut.GetPixel(x, y));
+                }
+            }
+        }
+
+        private void PutRight(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + 16 + (outSide ? 32 : 0), y));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + 16 + (outSide ? 32 : 0), y + 8, cut.GetPixel(x, 7 - y));
+                }
+            }
+        }
+
+        private void PutLeft(Bitmap read, Bitmap write, Boolean outSide = false)
+        {
+            Bitmap cut = new Bitmap(8, 8);
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    cut.SetPixel(x, y, read.GetPixel(x + 8 + (outSide ? 32 : 0), y));
+                }
+            }
+
+            cut.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            for (int y = 0; y < 8; ++y)
+            {
+                for (int x = 0; x < 8; ++x)
+                {
+                    write.SetPixel(x + (outSide ? 32 : 0), y + 8, cut.GetPixel(x, y));
+                }
+            }
+        }
+    }
+}
